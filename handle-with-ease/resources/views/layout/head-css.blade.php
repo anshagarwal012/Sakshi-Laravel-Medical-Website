c49d964b4f48f3d4ea23699e852ea7d43f289e93
Abhishek Singh <abhisheksingh.aks74@gmail.com>
2024-01-26 19:09:11 +0530

    master file ready

---
+++ handle-with-ease/resources/views/layout/head-css.blade.php
@@ -0,0 +1,26 @@
+<link rel="shortcut icon" href="{{ URL::asset('assets/images/site_logo/favourite_icon.svg')}}">
+
+<!-- Fraimwork - CSS Include -->
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/bootstrap.min.css')}}">
+
+<!-- Icon Font - CSS Include -->
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/fontawesome.min.css')}}">
+
+<!-- Animation - CSS Include -->
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/animate.min.css')}}">
+
+<!-- Cursor - CSS Include -->
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/cursor.min.css')}}">
+
+<!-- Carousel - CSS Include -->
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/slick.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/slick-theme.min.css')}}">
+
+<!-- Video & Image Popup - CSS Include -->
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/magnific-popup.min.css')}}">
+
+<!-- Counter - CSS Include -->
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/odometer.min.css')}}">
+
+<!-- Custom - CSS Include -->
+<link rel="stylesheet" type="text/css" href="{{ URL::asset('assets/css/style.css')}}">
