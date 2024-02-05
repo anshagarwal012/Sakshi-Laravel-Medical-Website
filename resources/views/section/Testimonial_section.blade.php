ea243007c08f053f0c2de14064fe0cc3788b5a1f
Abhishek Singh <abhisheksingh.aks74@gmail.com>
2024-02-05 13:13:57 +0530

    new upadated

--- resources/views/section/Testimonial_section.blade.php
+++ resources/views/section/Testimonial_section.blade.php
@@ -20,18 +20,19 @@
                         </ul>
                         <div class="author_box">
                             <div class="author_box_image">
-                                <img src="assets/images/meta/author_image_1.png"
-                                    alt="Reviewer Image - Talking Minds - Psychotherapist Site Template">
+                                <!--img src="assets/images/meta/author_image_1.png"
+                                    alt="Reviewer Image - Talking Minds - Psychotherapist Site Template"-->
                             </div>
                             <div class="author_box_content">
-                                <h3 class="author_box_name">Kerry Banks</h3>
-                                <span class="author_box_designation">Housewife</span>
+                                <h3 class="author_box_name">Arshia Tabassum</h3>
+                                <span class="author_box_designation">Patient</span>
                             </div>
                         </div>
                         <p class="mb-0">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting text industry. Lorem Ipsum
-                            has been the industry's standard dumm ever since the 1500s, when an unknown printer took a
-                            galley of type and scrambled it to make a type specimen book.
+                            Dr.Sakshi was giving physiotherapy to my mother who has lung infection. She was very polite
+                            and friendly to my mother. She knows how to handle senior citizens. My mother was very
+                            comfortable with her. My mother's physical condition had improved because of Dr.
+                            Sakshi's physiotherapy..
                         </p>
                     </div>
                 </div>
@@ -46,18 +47,18 @@
                         </ul>
                         <div class="author_box">
                             <div class="author_box_image">
-                                <img src="assets/images/meta/author_image_1.png"
-                                    alt="Reviewer Image - Talking Minds - Psychotherapist Site Template">
+                                <!--img src="assets/images/meta/author_image_1.png"
+                                    alt="Reviewer Image - Talking Minds - Psychotherapist Site Template"-->
                             </div>
                             <div class="author_box_content">
-                                <h3 class="author_box_name">Damian York</h3>
-                                <span class="author_box_designation">Entrepreneur</span>
+                                <h3 class="author_box_name">Deepak Sharma</h3>
+                                <span class="author_box_designation">Patient</span>
                             </div>
                         </div>
                         <p class="mb-0">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                            been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-                            galley of type text of the printing and typesetting industry.
+                            Highly recommend this physiotherapy She is friendly and professional, and the
+                            facility is clean and well-equipped. After just a few sessions, I already feel a noticeable
+                            improvement in my mobility and strength
                         </p>
                     </div>
                 </div>
@@ -72,18 +73,18 @@
                         </ul>
                         <div class="author_box">
                             <div class="author_box_image">
-                                <img src="assets/images/meta/author_image_1.png"
-                                    alt="Reviewer Image - Talking Minds - Psychotherapist Site Template">
+                                <!--img src="assets/images/meta/author_image_1.png"
+                                    alt="Reviewer Image - Talking Minds - Psychotherapist Site Template"-->
                             </div>
                             <div class="author_box_content">
-                                <h3 class="author_box_name">Kerry Banks</h3>
-                                <span class="author_box_designation">Housewife</span>
+                                <h3 class="author_box_name">Abhay Singh</h3>
+                                <span class="author_box_designation">Patient</span>
                             </div>
                         </div>
                         <p class="mb-0">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting text industry. Lorem Ipsum
-                            has been the industry's standard dumm ever since the 1500s, when an unknown printer took a
-                            galley of type and scrambled it to make a type specimen book.
+                            Outstanding physiotherapy!.They combine professionalism with a personal touch, ensuring that
+                            every session is effective and enjoyable. I appreciate the holistic approach they take to
+                            address both my physical and emotional well-being.
                         </p>
                     </div>
                 </div>
@@ -98,18 +99,18 @@
                         </ul>
                         <div class="author_box">
                             <div class="author_box_image">
-                                <img src="assets/images/meta/author_image_1.png"
-                                    alt="Reviewer Image - Talking Minds - Psychotherapist Site Template">
+                                <!--img src="assets/images/meta/author_image_1.png"
+                                    alt="Reviewer Image - Talking Minds - Psychotherapist Site Template"-->
                             </div>
                             <div class="author_box_content">
-                                <h3 class="author_box_name">Damian York</h3>
-                                <span class="author_box_designation">Entrepreneur</span>
+                                <h3 class="author_box_name">Himanshu Verma</h3>
+                                <span class="author_box_designation">Patient</span>
                             </div>
                         </div>
                         <p class="mb-0">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                            been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-                            galley of type text of the printing and typesetting industry.
+                            Five-star experience! From the moment I walked in, I felt welcomed and supported. The
+                            therapist here are truly dedicated to helping their Patients achieve their goals. I'm
+                            grateful for the progress I've made under their guidance.
                         </p>
                     </div>
                 </div>
@@ -124,18 +125,19 @@
                         </ul>
                         <div class="author_box">
                             <div class="author_box_image">
-                                <img src="assets/images/meta/author_image_1.png"
-                                    alt="Reviewer Image - Talking Minds - Psychotherapist Site Template">
+                                <!--img src="assets/images/meta/author_image_1.png"
+                                    alt="Reviewer Image - Talking Minds - Psychotherapist Site Template"-->
                             </div>
                             <div class="author_box_content">
-                                <h3 class="author_box_name">Kerry Banks</h3>
-                                <span class="author_box_designation">Housewife</span>
+                                <h3 class="author_box_name">Abhishek Tripathi</h3>
+                                <span class="author_box_designation">Patient</span>
                             </div>
                         </div>
                         <p class="mb-0">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting text industry. Lorem Ipsum
-                            has been the industry's standard dumm ever since the 1500s, when an unknown printer took a
-                            galley of type and scrambled it to make a type specimen book.
+                            After struggling with chronic pain for months, I finally decided to try physiotherapy, and
+                            I'm so glad I did. The therapist here are skilled at identifying the root cause of my
+                            discomfort and providing targeted treatment. I'm already feeling stronger and more confident
+                            in my body.
                         </p>
                     </div>
                 </div>
@@ -150,18 +152,18 @@
                         </ul>
                         <div class="author_box">
                             <div class="author_box_image">
-                                <img src="assets/images/meta/author_image_1.png"
-                                    alt="Reviewer Image - Talking Minds - Psychotherapist Site Template">
+                                <!--img src="assets/images/meta/author_image_1.png"
+                                    alt="Reviewer Image - Talking Minds - Psychotherapist Site Template"-->
                             </div>
                             <div class="author_box_content">
-                                <h3 class="author_box_name">Damian York</h3>
-                                <span class="author_box_designation">Entrepreneur</span>
+                                <h3 class="author_box_name">Saket Yadav</h3>
+                                <span class="author_box_designation">Patient</span>
                             </div>
                         </div>
                         <p class="mb-0">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                            been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-                            galley of type text of the printing and typesetting industry.
+                            Impressive results! I've been struggling with post-surgery recovery, but thanks to the
+                            guidance of the skilled therapist, I'm making significant progress. Their expertise and
+                            encouragement have been invaluable on my road to recovery.
                         </p>
                     </div>
                 </div>
