4a2579111e5bcca3d2b356e847c6f5e884297c20
Abhishek Singh <148897811+Abhi7251singh@users.noreply.github.com>
2024-02-06 14:47:02 +0530

    some changes done

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -1,4 +1,5 @@
 @extends('layout.master')
 @section('content')
     @include('section.Consultation_section_2')
+    @include('section.social_link')
 @endsection
---
+++ resources/views/section/social_link.blade.php
@@ -0,0 +1,13 @@
+<div class="container d-flex justify-content-center align-items-center mb-5 gap-2">
+    <h3 class="social_title mb-0">Follow Us:</h3>
+    <br>
+    <ul class="social_links unordered_list">
+        <li><a class="bg-primary" href="https://www.facebook.com/handlewitheaseofficial"><i
+                    class="fa-brands fa-facebook-f"></i></a></li>
+        <li><a class="bg-primary" href="https://www.instagram.com/handle_with_ease/"><i
+                    class="fa-brands fa-instagram"></i></a></li>
+        <!--li><a class="bg-primary" href="#!"><i class="fa-brands fa-twitter"></i></a></li-->
+        <li><a class="bg-primary" href="https://api.whatsapp.com/send/?phone=918299626136"><i
+                    class="fa-brands fa-whatsapp"></i></a></li>
+    </ul>
+</div>
