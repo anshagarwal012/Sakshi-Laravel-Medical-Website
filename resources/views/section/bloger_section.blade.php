d2fb5d017a08860d15e73315091d6755de82b51d
Ansh Agarwal <info.ansh012@gmail.com>
2024-03-01 12:20:08 +0530

    blog fix

--- resources/views/section/bloger_section.blade.php
+++ resources/views/section/bloger_section.blade.php
@@ -23,7 +23,7 @@
                 <div class="col-lg-4 col-md-6 col-sm-6">
                     <div class="blog_item">
                         <div class="blog_image">
-                            <a class="blog_image_wrap" href="/blogs/{{ \Str::slug($cat['name']) }}">
+                            <a class="blog_image_wrap" href="/blogs/{{$cat['id']}}/{{ \Str::slug($cat['name']) }}">
                                 <img src="{{ $cat['image'] }}" alt="{{ $cat['name'] }}">
                             </a>
                         </div>
@@ -35,10 +35,10 @@
                                 <li>{{ $cat['created_at'] }}</li>
                             </ul>
                             <h3 class="item_title">
-                                <a href="/blogs/{{ \Str::slug($cat['name']) }}">{{ $cat['name'] }}</a>
+                                <a href="/blogs/{{$cat['id']}}/{{ \Str::slug($cat['name']) }}">{{ $cat['name'] }}</a>
                             </h3>
                             <p>{{ $cat['desc'] }}</p>
-                            <a class="btn-link" href="/blogs/{{ \Str::slug($cat['name']) }}">
+                            <a class="btn-link" href="/blogs/{{$cat['id']}}/{{ \Str::slug($cat['name']) }}">
                                 <span class="btn_text">Read More</span>
                                 <span class="btn_icon"><i class="fa-solid fa-arrow-up-right"></i></span>
                             </a>
